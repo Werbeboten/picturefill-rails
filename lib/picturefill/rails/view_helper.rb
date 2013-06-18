671e09a79825abbbacba6e459ed0bd03b4346a87
Patrick Helm <deradon87@gmail.com>
2013-06-19 00:21:09 +0200

    Fixes: view_helper.rb

--- lib/picturefill/rails/view_helper.rb
+++ lib/picturefill/rails/view_helper.rb
@@ -1,13 +1,13 @@
-# Usage: <%= picturefill(default_url, "AltText") do %>
-#          <%= image(small_url) %>
-#          <%= image(medium_url, :min => 400) %>
-#          <%= image(medium_high_dpi_url, :min => 400, :ratio => 1.5) %>
-#          <%= image(large_url, :min => 1000) %>
-#          <%= image(large_high_dpi_url, "(min-width: 1000px) and (min-device-pixel-ratio: 1.5)") %>
-#        <%= end %>
 module Picturefill::Rails
   module ViewHelper
 
+    # Usage: <%= picturefill(default_url, "AltText") do %>
+    #          <%= image(small_url) %>
+    #          <%= image(medium_url, :min => 400) %>
+    #          <%= image(medium_high_dpi_url, :min => 400, :ratio => 1.5) %>
+    #          <%= image(large_url, :min => 1000) %>
+    #          <%= image(large_high_dpi_url, "(min-width: 1000px) and (min-device-pixel-ratio: 1.5)") %>
+    #        <%= end %>
     def picturefill(default_src, description, &block)
       raise ArgumentError.new("No Block given") unless block_given?
 
