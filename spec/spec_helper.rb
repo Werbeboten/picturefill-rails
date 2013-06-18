fa3672fc4626647d53e9579cc02d71b25db34483
Patrick Helm <deradon87@gmail.com>
2013-06-19 00:06:21 +0200

    Fixes: spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,9 @@
 ENV["RAILS_ENV"] = "test"
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter "spec/dummy"
+end
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require 'rspec/rails'
